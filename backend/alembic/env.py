68908ea9cee30205ce9e939b13279f19d8884265
Tanish9022 <tanishdhende24@gmail.com>
2026-05-30 22:59:53 +0530

    Configure Alembic to dynamically load DATABASE_URL from env variables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,6 +9,13 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    # Alembic/SQLAlchemy requires postgresql:// instead of postgres:// from some providers (like Heroku), 
+    # but Neon provides postgresql:// so this is clean.
+    config.set_main_option("sqlalchemy.url", db_url)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
